717cedac09a445b4659bb711dd5de58176f4db99
Darkatse <39111369+Darkatse@users.noreply.github.com>
2024-11-08 19:23:08 +0800

    add anti-aliasing

--- src/components/three/renderer.ts
+++ src/components/three/renderer.ts
@@ -2,7 +2,8 @@
 import * as THREE from 'three';
 
 export function initRenderer(container: HTMLElement): THREE.WebGLRenderer {
-  const renderer = new THREE.WebGLRenderer();
+  // add antialias in default
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setClearColor(0xffffff, 1);
   container.appendChild(renderer.domElement);

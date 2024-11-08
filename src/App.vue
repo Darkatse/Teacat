160f26643affcf11c1107018051a7f3ff0d0c921
Darkatse <39111369+Darkatse@users.noreply.github.com>
2024-11-08 19:23:19 +0800

    hidden scrollbar

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,10 @@ export default {
 
 <style>
 #app {
-  width: 100vw;
-  height: 100vh;
+  /* hidden scrollbar */
+	position: fixed;
+	width: 100%;
+	height: 100%;
   overflow: hidden;
   background-color: #ffffff;
 }
